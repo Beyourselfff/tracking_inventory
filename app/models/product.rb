002ab3797c9dc93c90f1540e39c634565b9aa7aa
Jesus Castilla <jesus.jcg8@gmail.com>
2022-05-12 12:56:59 -0500

    Placeholder, validates and create_product link added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :code, presence: true, confirmation: true
+  validates :stock, presence: true
+  validates :category, presence: true
 end
